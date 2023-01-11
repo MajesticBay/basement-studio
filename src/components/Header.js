60516df7314c7bf54ac9cd1a66317e7c71898705
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-11 00:38:09 -0800

    Work on Header: styling and layout.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
 import Hamburger from "./Hamburger";
 import logo from "../images/logo.svg";
+import down from "../images/down.svg";
 
 export const Header = () => {
     return (
         <header className="header">
-            <nav className="nav__mobile">
+            <nav className="navigation-mobile">
                 <img src={logo} alt="hamburger menu" />
                 <Hamburger />
             </nav>
-            <nav className="nav__desktop">
+            <nav className="navigation">
                 <img src={logo} alt="hamburger menu" />
-                <ul className="nav-desktop__list-menu">
-                    <li>ПРО НАС</li>
-                    <li>СТУДІЯ ЗВУКОЗАПИСУ</li>
-                    <li>КУРС DJ</li>
-                    <li>КУРС PRODUCTION</li>
-                    <li>ОРЕНДА</li>
-                    <li>ПОДКАСТ</li>
-                    <li>КОНТАКТИ</li>
+                <ul className="nav">
+                    <li className="nav__menu-item">ПРО НАС</li>
+                    <li className="nav__menu-item">СТУДІЯ ЗВУКОЗАПИСУ</li>
+                    <li className="nav__menu-item">КУРС DJ</li>
+                    <li className="nav__menu-item">КУРС PRODUCTION</li>
+                    <li className="nav__menu-item">ОРЕНДА</li>
+                    <li className="nav__menu-item">ПОДКАСТ</li>
+                    <li className="nav__menu-item">КОНТАКТИ</li>
                 </ul>
-                <p></p>
+                <div className="locale">
+                    <p className="locale__current-language">UA</p>
+                    <img src={down} className="locale__down" />
+                </div>
             </nav>
         </header>
     )
