0f3f5c91c6753fded6b15228838fb66e0982fdb1
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-11 17:52:11 -0800

    Work on Top Screen styling and layout: add a background image.

--- src/components/TopScreen.js
+++ src/components/TopScreen.js
@@ -3,10 +3,13 @@ import bgMobile from "../images/bg-mobile.png";
 
 export const TopScreen = () => {
     return (
-        <div style={{
-            backgroundImage:`url(${bgMobile})`,
-            height: `100vh`
-            }}>
+        <div className="top-screen"
+            //  className="top-screen" style={{
+            //  backgroundImage:`url(${bgMobile})`,
+            //  height: `100vh`
+            //  }}
+        >
+            123
         </div>
     )
 }
