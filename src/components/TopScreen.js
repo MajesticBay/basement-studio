3535fc19f38bd28b667433d07436a98377c0d20e
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-11 19:42:26 -0800

    Add a mouse scroll image to the top screen.

--- src/components/TopScreen.js
+++ src/components/TopScreen.js
@@ -1,15 +1,11 @@
-
-import bgMobile from "../images/bg-mobile.png";
+import mouse from "../images/scroll.svg";
 
 export const TopScreen = () => {
     return (
-        <div className="top-screen"
-            //  className="top-screen" style={{
-            //  backgroundImage:`url(${bgMobile})`,
-            //  height: `100vh`
-            //  }}
-        >
-            123
+        <div className="top-screen">
+            <div className="top-screen__mouse-container">
+                <img src={mouse} alt="mouse" />
+            </div>
         </div>
     )
 }
