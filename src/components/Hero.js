5b60965029f55adc0140a5c86b5d728c43542443
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-11 20:01:42 -0800

    Work on responsiveness: show different scroll images depending on screen size.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
-import mouse from "../images/scroll.svg";
+import scrollMobile from "../images/scroll-mobile.svg";
+import scrollDesktop from "../images/scroll-desktop.svg";
 
 export const Hero = () => {
     return (
-        <div className="hero-screen">
-            <div className="hero-screen__mouse-container">
-                <img src={mouse} alt="mouse" />
+        <div className="hero">
+            <div className="hero__scroll-container">
+                <img className="hero__scroll hero__scroll--mobile" src={scrollMobile} alt="scroll mobile" />
+                <img className="hero__scroll hero__scroll--desktop" src={scrollDesktop} alt="scroll desktop" />
             </div>
         </div>
     )
