a04b6544653175dd14907f29aed2c27610c3cd41
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-14 01:44:30 -0800

    Work on card layout and style in RecordStudio component.

--- src/components/RecordStudio.js
+++ src/components/RecordStudio.js
@@ -4,6 +4,16 @@ export const RecordStudio = () => {
             <h1 className="record-studio__header">СТУДІЯ  ЗВУКОЗАПИСУ</h1>
             <p className="record-studio__text">Атмосфера в студії дозволить насолоджуватись процесом звукозапису або музичного продакшену.</p>
             <p className="record-studio__text">Звукорежисер - твій друг, професіонал, який завжди підтримає, порадить і допоможе зробити процес запису максимально комфортним і приємним.</p>
+            <div className="card">
+                <p className="card__title">ЗВУКОЗАПИС</p>
+                <p className="card__price">14$ / год</p>
+                <ul className="card__list">
+                    <li className="card__text">Запис вокалу, інструментів та аудіоподкастів за допомогою професійного обладнання.</li>
+                    <li className="card__text">Дикторське озвучення для реклами та різного роду відеоконтенту.</li>
+                    <li className="card__text">Виготовлення аудіокниг та дубляж кіно.</li>
+                </ul>
+                <p className="card__button">ЗАМОВИТИ</p>
+            </div>
         </div>
     )
 }
