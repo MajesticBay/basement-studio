a33ae8056fd8ea183841374caaefdec7c8927e5d
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-16 22:07:06 -0800

    Start working on podcast; create layout and styling.

---
+++ src/components/Podcast.js
@@ -0,0 +1,10 @@
+export const Podcast = () => {
+    return (
+        <div>
+            <h1>ПОДКАСТ</h1>
+            <div>
+                <p>It's all about unity of underground music culture.</p>
+            </div>
+        </div>
+    )
+}
