28d5d9fd4a186e005ebf171597f1161fbda39618
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-28 15:08:15 -0800

    Make changes to the button component: make it so that we can style it more using the props.

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,11 +1,28 @@
 import arrow from "../images/arrow.svg";
+import styled from "styled-components";
 
-export const Btn = ({ text, arrowDisplayed }) => {
+const Button = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.8rem 0;
+    background-color: ${(({ dark }) => (dark ? "transparent" : "white"))};
+    border: 2px solid rgba(255, 255, 255, 0.4);
+    // color: var(--color-white);
+    color: ${(({ dark }) => (dark ? "white" : "black"))};
+    font-weight: 800;
+}`
+
+export const Btn = ({ text, arrowDisplayed, dark }) => {
+    // dark = dark;
+    JSON.stringify(dark);
+    console.log("dark ", dark);
     return (
-        <div className="btn pointer">
+        <Button dark={dark} className="pointer">
             <span>{ text }</span>
             { arrowDisplayed ? <img src={arrow} alt="arrow" /> : <></> }
-        </div>
+        </Button>
     )
 }
 
