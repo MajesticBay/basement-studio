0e7c237813c6b088e959c13363f8f2b84953c491
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-30 09:08:48 -0800

    Change content for 2 cards in RecordStudio.

--- src/components/RecordStudio.js
+++ src/components/RecordStudio.js
@@ -29,11 +29,11 @@ export const RecordStudio = () => {
                 </div>
                 <div className="card">
                     <p className="card__title">ЗВУКОЗАПИС</p>
-                    <p className="card__price">14$ / год</p>
+                    <p className="card__price">70$ / год</p>
                     <ul className="card__list">
-                        <li className="card__text">Запис вокалу, інструментів та аудіоподкастів за допомогою професійного обладнання.</li>
-                        <li className="card__text">Дикторське озвучення для реклами та різного роду відеоконтенту.</li>
-                        <li className="card__text">Виготовлення аудіокниг та дубляж кіно.</li>
+                        <li className="card__text">Зведення та мастерінг твоєї музики професійним та досвідченим звукорежисером.</li>
+                        <li className="card__text">Послуги зведення та мастерингу онлайн.</li>
+                        <li className="card__text">Термін виконання 1-3 робочі дні.</li>
                     </ul>
                     <Btn
                         text={"ЗАМОВИТИ"}
@@ -43,11 +43,10 @@ export const RecordStudio = () => {
                 </div>
                 <div className="card">
                     <p className="card__title">ЗВУКОЗАПИС</p>
-                    <p className="card__price">14$ / год</p>
+                    <p className="card__price">80$ / год</p>
                     <ul className="card__list">
-                        <li className="card__text">Запис вокалу, інструментів та аудіоподкастів за допомогою професійного обладнання.</li>
-                        <li className="card__text">Дикторське озвучення для реклами та різного роду відеоконтенту.</li>
-                        <li className="card__text">Виготовлення аудіокниг та дубляж кіно.</li>
+                        <li className="card__text">Ексклюзивний продакшн твого треку в будь-якому стилі. Приходь на студійну сесію та приймай безпосередню участь у творчому процесі.</li>
+                        <li className="card__text">Саунд дизайн та музика для реклами, кіно та різного роду відеоконтенту.</li>
                     </ul>
                     <Btn
                         text={"ЗАМОВИТИ"}
