98c972afefb8338bcd6032ab6a6f4d5a69d1525a
Sergey Sherstobitov <in43sh@gmail.com>
2023-01-30 00:52:56 -0800

    Style ContactUs component.

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -9,17 +9,17 @@ import { Btn } from "./Btn";
 export const ContactUs = () => {
     return (
         <div className="contact-us">
-            <h1 className="contact-us__header">КОНТАКТИ</h1>
+            <h1 className="header">КОНТАКТИ</h1>
             <div className="contact-us__links-container">
-                <a href="#email">
+                <a className="contact-us__link" href="#email">
                     <img src={email} alt="email" />
                     <span className="contact-us__text">basementstudio@gmail.com</span>
                 </a>
-                <a href="#phonemail">
+                <a className="contact-us__link" href="#phonemail">
                     <img src={phone} alt="phone" />
                     <span className="contact-us__text">+380XXXXXXXXX</span>
                 </a>
-                <a href="#address">
+                <a className="contact-us__link" href="#address">
                     <img src={address} alt="address" />
                     <span className="contact-us__text">вул. Сахарова 2, м. Львів, 79026</span>
                 </a>
