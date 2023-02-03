cdfc508e9ec7347bd8527bc96f6d06c327a1c351
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-03 02:16:51 -0800

    Map through cards data and display them dynamically instead of statically showing them.

--- src/components/RecordStudio.js
+++ src/components/RecordStudio.js
@@ -7,16 +7,69 @@ export const RecordStudio = () => {
         background: "purple"
     };
 
+    const cardsData = [
+        {
+            title: "ЗВУКОЗАПИС",
+            price: "14",
+            list: [
+                "Запис вокалу, інструментів та аудіоподкастів за допомогою професійного обладнання.",
+                "Дикторське озвучення для реклами та різного роду відеоконтенту.",
+                "Виготовлення аудіокниг та дубляж кіно."
+            ]
+        },
+        {
+            title: "ЗВЕДЕННЯ ТА МАСТЕРІНГ",
+            price: "70",
+            list: [
+                "Зведення та мастерінг твоєї музики професійним та досвідченим звукорежисером.",
+                "Послуги зведення та мастерингу онлайн.",
+                "Термін виконання 1-3 робочі дні."
+            ]
+        },
+        {
+            title: "МУЗИЧНИЙ ПРОДАКШН",
+            price: "80",
+            list: [
+                "Ексклюзивний продакшн твого треку в будь-якому стилі. Приходь на студійну сесію та приймай безпосередню участь у творчому процесі.",
+                "Саунд дизайн та музика для реклами, кіно та різного роду відеоконтенту."
+            ]
+        }
+    ]
+
+    const cards = cardsData.map((card) => (
+            <div className="card">
+                {/* <p>12123123123123123SDFSDFSD</p> */}
+                {/* ------------------------------- */}
+                {/* TO DO */}
+                {/* Give each element inside the card a key */}
+                {/* ------------------------------- */}
+                <p className="card__title">{ card.title }</p>
+                <p className="card__price">{ card.price }$ / год</p>
+                <ul className="card__list">
+                    {card.list.map((item) => (
+                        <li className="card__list-item">{item}</li>
+                    ))}
+                </ul>
+                <Btn
+                    className={"record-studio__btn"}
+                    text={"ЗАМОВИТИ"}
+                    arrowDisplayed={false}
+                    dark={false}
+                />
+            </div>
+    ))
+
     return (
         <div className="record-studio" style={{bgStyle}}>
             <h1 className="header">СТУДІЯ  ЗВУКОЗАПИСУ</h1>
             <p className="record-studio__text">Атмосфера в студії дозволить насолоджуватись процесом звукозапису або музичного продакшену.</p>
             <p className="record-studio__text">Звукорежисер - твій друг, професіонал, який завжди підтримає, порадить і допоможе зробити процес запису максимально комфортним і приємним.</p>
             <div className="cards">
-                <div className="card">
+                {cards}
+                {/* <div className="card">
                     <p className="card__title">ЗВУКОЗАПИС</p>
                     <p className="card__price">14$ / год</p>
-                    <ul className="card__list">
+                    <ul>
                         <li className="card__list-item">Запис вокалу, інструментів та аудіоподкастів за допомогою професійного обладнання.</li>
                         <li className="card__list-item">Дикторське озвучення для реклами та різного роду відеоконтенту.</li>
                         <li className="card__list-item">Виготовлення аудіокниг та дубляж кіно.</li>
@@ -56,7 +109,7 @@ export const RecordStudio = () => {
                         arrowDisplayed={false}
                         dark={false}
                     />
-                </div>
+                </div> */}
             </div>
         </div>
     )
