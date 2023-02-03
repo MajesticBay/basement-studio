4cfaf0decc7487f9c9bb764141f26822ba463e04
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-03 01:48:48 -0800

    Add one more card to Rent.

--- src/components/GiftCertificate.js
+++ src/components/GiftCertificate.js
@@ -5,7 +5,7 @@ export const GiftCertificate = () => {
     return (
         <div className="gift-certificate">
             <img src={ giftCertificate } alt="gift certificate" />
-            <h1 className="gift-certificate__header header">ПОДАРУНКОВИЙ СЕРТИФІКАТ</h1>
+            <h1 className="gift-certificate__header header mobile">ПОДАРУНКОВИЙ СЕРТИФІКАТ</h1>
             <p className="gift-certificate__text">Текст про те яким класним подарунком може бути сертифікат на послуги студії)))</p>
             <div className="gift-certificate__btn-container">
                 <Btn text={"ЗАМОВИТИ"} arrowDisplayed={false} dark={false} />
--- src/components/Rent.js
+++ src/components/Rent.js
@@ -5,20 +5,37 @@ export const Rent = () => {
         <div className="rent">
             <h1 className="header">ОРЕНДА</h1>
             <div className="rent__card-container">
-                <div className="card">
-                    <p className="card__title">DJ PLACE</p>
-                    <p className="card__price">13$ / год</p>
-                    <ul className="card__list">
-                        <li className="card__text">Практикуй та записуй свій dj мікс на топовому обладнанні, що є клубним сандартом. Включає якісний запис твого мікса.</li>
-                        <li className="card__text">2x CDJ-300 | Xone:96 | 2x Technics SL-1210MK2 | 2x Ortofon Nightclub E.</li>
-                        <li className="card__text">Universal Audio Apollo Twin для якісного запису твого dj міксу або лайву.</li>
-                    </ul>
-                    <Btn
-                        className={"record-studio__btn"}
-                        text={"ЗАМОВИТИ"}
-                        arrowDisplayed={false}
-                        dark={false}
-                    />
+                <div className="cards">
+                    <div className="card">
+                        <p className="card__title">DJ PLACE</p>
+                        <p className="card__price">13$ / год</p>
+                        <ul className="card__list">
+                            <li className="card__list-item">Практикуй та записуй свій dj мікс на топовому обладнанні, що є клубним сандартом. Включає якісний запис твого мікса.</li>
+                            <li className="card__list-item">2x CDJ-300 | Xone:96 | 2x Technics SL-1210MK2 | 2x Ortofon Nightclub E.</li>
+                            <li className="card__list-item">Universal Audio Apollo Twin для якісного запису твого dj міксу або лайву.</li>
+                        </ul>
+                        <Btn
+                            className={"record-studio__btn"}
+                            text={"ЗАМОВИТИ"}
+                            arrowDisplayed={false}
+                            dark={false}
+                        />
+                    </div>
+                    <div className="card">
+                        <p className="card__title">PRODUCTION PLACE</p>
+                        <p className="card__price">13$ / год</p>
+                        <ul className="card__list">
+                            <li className="card__list-item">Експерементуй з аналоговим обладнанням та DAW;</li>
+                            <li className="card__list-item">Міксуй та роби мастерінг своїх треків;</li>
+                            <li className="card__list-item">Записуй вокал, шуми та звукові ефекти в рекордрумі.</li>
+                        </ul>
+                        <Btn
+                            className={"record-studio__btn"}
+                            text={"ЗАМОВИТИ"}
+                            arrowDisplayed={false}
+                            dark={false}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
