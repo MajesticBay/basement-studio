2109b8431de4fc0131f70b040298620c8df1d85f
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-07 10:57:58 -0800

    Start working on ContactUs desktop version.

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -9,28 +9,33 @@ import { Btn } from "./Btn";
 export const ContactUs = () => {
     return (
         <div className="contact-us">
-            <h1 className="header">КОНТАКТИ</h1>
-            <div className="contact-us__links-container">
-                <a className="contact-us__link" href="#email">
-                    <img src={email} alt="email" />
-                    <span className="contact-us__text">basementstudio@gmail.com</span>
-                </a>
-                <a className="contact-us__link" href="#phonemail">
-                    <img src={phone} alt="phone" />
-                    <span className="contact-us__text">+380XXXXXXXXX</span>
-                </a>
-                <a className="contact-us__link" href="#address">
-                    <img src={address} alt="address" />
-                    <span className="contact-us__text">вул. Сахарова 2, м. Львів, 79026</span>
-                </a>
+            <div className="contact-us__left">
+                <h1 className="contact-us__header header">КОНТАКТИ</h1>
+                <div className="contact-us__links-container">
+                    <a className="contact-us__link" href="#email">
+                        <img src={email} alt="email" />
+                        <span className="contact-us__text">basementstudio@gmail.com</span>
+                    </a>
+                    <a className="contact-us__link" href="#phonemail">
+                        <img src={phone} alt="phone" />
+                        <span className="contact-us__text">+380XXXXXXXXX</span>
+                    </a>
+                    <a className="contact-us__link" href="#address">
+                        <img src={address} alt="address" />
+                        <span className="contact-us__text">вул. Сахарова 2, м. Львів, 79026</span>
+                    </a>
+                </div>
+                <div className="contact-us__social-media-container">
+                    <IconFB className="pointer" />
+                    <IconIG className="pointer" />
+                    <IconTK className="pointer" />
+                </div>
+                <h1 className="contact-us__sub-header">ЗАЛИШИЛИСЯ ЗАПИТАННЯ?</h1>
+                <Btn text={"ЗВ’ЯЖІТЬСЯ З НАМИ"} arrowDisplayed={true} dark={false} />
             </div>
-            <div className="contact-us__social-media-container">
-                <IconFB className="pointer" />
-                <IconIG className="pointer" />
-                <IconTK className="pointer" />
+            <div className="contact-us__right">
+
             </div>
-            <h1 className="contact-us__sub-header">ЗАЛИШИЛИСЯ ЗАПИТАННЯ?</h1>
-            <Btn text={"ЗВ’ЯЖІТЬСЯ З НАМИ"} arrowDisplayed={true} dark={false} />
         </div>
     )
 }
