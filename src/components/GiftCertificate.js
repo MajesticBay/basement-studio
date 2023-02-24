a58d3c3a36bd60fd9a6372d861aeff15cbfeca75
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-24 03:19:59 -0800

    Style GiftCertificate.

--- src/components/GiftCertificate.js
+++ src/components/GiftCertificate.js
@@ -4,12 +4,14 @@ import { Btn } from "./Btn";
 export const GiftCertificate = () => {
     return (
         <div className="gift-certificate">
-            <img src={ giftCertificate } alt="gift certificate" />
-            <h1 className="gift-certificate__header header mobile">ПОДАРУНКОВИЙ СЕРТИФІКАТ</h1>
-            <p className="gift-certificate__text">Текст про те яким класним подарунком може бути сертифікат на послуги студії)))</p>
-            <div className="gift-certificate__btn-container">
-                <Btn text={"ЗАМОВИТИ"} arrowDisplayed={false} dark={false} />
-                <Btn text={"ДІЗНАТИСЯ БІЛЬШЕ"} arrowDisplayed={true} dark={true} />
+            <div className="gift-certificate__inner">
+                <img src={ giftCertificate } alt="gift certificate" />
+                <h1 className="gift-certificate__header header mobile">ПОДАРУНКОВИЙ СЕРТИФІКАТ</h1>
+                <p className="gift-certificate__text">Текст про те яким класним подарунком може бути сертифікат на послуги студії)))</p>
+                <div className="gift-certificate__btn-container">
+                    <Btn text={"ЗАМОВИТИ"} arrowDisplayed={false} dark={false} />
+                    <Btn text={"ДІЗНАТИСЯ БІЛЬШЕ"} arrowDisplayed={true} dark={true} />
+                </div>
             </div>
         </div>
     )
