b840ce41d3b05cbd422cb5b071d363d82a5948cd
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-25 13:04:38 -0800

    Make contact us container css grid instead of flexbox.

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,4 +1,4 @@
-// import { MapComponent } from "./MapComponent.tsx";
+import { MapComponent } from "./MapComponent.tsx";
 import email from "../images/icons/email.svg";
 import phone from "../images/icons/phone.svg";
 import address from "../images/icons/address.svg";
@@ -10,7 +10,7 @@ import { Btn } from "./Btn";
 export const ContactUs = () => {
     return (
         <div className="contact-us">
-            <div className="contact-us__left">
+            {/* <div className="contact-us__left">
                 <h1 className="contact-us__header header">КОНТАКТИ</h1>
                 <div className="contact-us__links-container">
                     <a className="contact-us__link" href="#email">
@@ -35,8 +35,33 @@ export const ContactUs = () => {
                 <Btn text={"ЗВ’ЯЖІТЬСЯ З НАМИ"} arrowDisplayed={true} dark={false} />
             </div>
             <div className="contact-us__right">
-                {/* <MapComponent /> */}
-            </div>
+                <MapComponent />
+            </div> */}
+                <h1 className="contact-us__header header">КОНТАКТИ</h1>
+                <div className="contact-us__links-container">
+                    <a className="contact-us__link" href="#email">
+                        <img src={email} alt="email" />
+                        <span className="contact-us__text">basementstudio@gmail.com</span>
+                    </a>
+                    <a className="contact-us__link" href="#phonemail">
+                        <img src={phone} alt="phone" />
+                        <span className="contact-us__text">+380XXXXXXXXX</span>
+                    </a>
+                    <a className="contact-us__link" href="#address">
+                        <img src={address} alt="address" />
+                        <span className="contact-us__text">вул. Сахарова 2, м. Львів, 79026</span>
+                    </a>
+                </div>
+                <div className="contact-us__social-media-container">
+                    <IconFB className="pointer" />
+                    <IconIG className="pointer" />
+                    <IconTK className="pointer" />
+                </div>
+                <div className="contact-us__map map">
+                    <MapComponent />
+                </div>
+                <h1 className="contact-us__sub-header">ЗАЛИШИЛИСЯ ЗАПИТАННЯ?</h1>
+                <Btn className={"contact-us__sub-header"} text={"ЗВ’ЯЖІТЬСЯ З НАМИ"} arrowDisplayed={true} dark={false} />
         </div>
     )
 }
