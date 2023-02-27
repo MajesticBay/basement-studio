05a33a18eb1a95b001ee2b9ccf331a801abab9f9
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-27 10:06:18 -0800

    Style GiftCertificate component.

--- src/components/GiftCertificate.js
+++ src/components/GiftCertificate.js
@@ -1,16 +1,28 @@
-import giftCertificate from "../images/icons/gift-certificate.svg";
 import { Btn } from "./Btn";
+import bg from "../images/bg-record-studio-desktop.png";
+import giftCertificate from "../images/icons/gift-certificate.svg";
 
 export const GiftCertificate = () => {
+    const bgStyle = {
+        backgroundImage: `url(${bg})`
+    }
+
+    const bgOverlayStyle = {
+        background: "rgba(7, 7, 7, 0.6)",
+        backdropFilter: "blur(3px)"
+    }
+
     return (
-        <div className="gift-certificate">
-            <div className="gift-certificate__inner">
-                <img src={ giftCertificate } alt="gift certificate" />
-                <h1 className="gift-certificate__header header mobile">ПОДАРУНКОВИЙ СЕРТИФІКАТ</h1>
-                <p className="gift-certificate__text">Текст про те яким класним подарунком може бути сертифікат на послуги студії)))</p>
-                <div className="gift-certificate__btn-container">
-                    <Btn text={"ЗАМОВИТИ"} arrowDisplayed={false} dark={false} />
-                    <Btn text={"ДІЗНАТИСЯ БІЛЬШЕ"} arrowDisplayed={true} dark={true} />
+        <div className="gift-certificate" style={bgStyle}>
+            <div className="gift-certificate__bgOverlay" style={bgOverlayStyle}>
+                <div className="gift-certificate__inner">
+                    <img src={ giftCertificate } alt="gift certificate" />
+                    <h1 className="gift-certificate__header header mobile">ПОДАРУНКОВИЙ СЕРТИФІКАТ</h1>
+                    <p className="gift-certificate__text">Текст про те яким класним подарунком може бути сертифікат на послуги студії)))</p>
+                    <div className="gift-certificate__btn-container">
+                        <Btn text={"ЗАМОВИТИ"} arrowDisplayed={false} dark={false} />
+                        <Btn text={"ДІЗНАТИСЯ БІЛЬШЕ"} arrowDisplayed={true} dark={true} />
+                    </div>
                 </div>
             </div>
         </div>
