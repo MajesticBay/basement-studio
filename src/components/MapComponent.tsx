0f789418f01b4cc052619987a107b648533e896a
Sergey Sherstobitov <in43sh@gmail.com>
2023-02-27 21:39:16 -0800

    Change coordinates of the studio.

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -191,18 +191,18 @@ const exampleMapStyles = [
   }
 ];
 
-const latLngStudio =  { lat: 50.422925459083714, lng: 30.382006490209367 }
+const latLngStudio =  { lat: 50.442425221700354, lng: 30.63224526525359 }
 
 const render = (status: Status) => {
-  return <h1>{status}</h1>;
+  return <h1 style={{textAlign: "center"}}>{status}</h1>;
 };
 
 export const MapComponent: React.VFC = () => {
   const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
-  const [zoom, setZoom] = React.useState(11); // initial zoom
+  const [zoom, setZoom] = React.useState(11.4); // initial zoom
   const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
-    lat: 50.422925459083714,
-    lng: 30.382006490209367
+    lat: 50.442425221700354,
+    lng: 30.63224526525359
   });
 
   const onClick = (e: google.maps.MapMouseEvent) => {
@@ -216,55 +216,57 @@ export const MapComponent: React.VFC = () => {
     setCenter(m.getCenter()!.toJSON());
   };
 
-  const form = (
-    <div
-      style={{
-        padding: "1rem",
-        flexBasis: "250px",
-        height: "100%",
-        overflow: "auto"
-      }}
-    >
-      <label htmlFor="zoom">Zoom</label>
-      <input
-        type="number"
-        id="zoom"
-        name="zoom"
-        value={zoom}
-        onChange={(event) => setZoom(Number(event.target.value))}
-      />
-      <br />
-      <label htmlFor="lat">Latitude</label>
-      <input
-        type="number"
-        id="lat"
-        name="lat"
-        value={center.lat}
-        onChange={(event) =>
-          setCenter({ ...center, lat: Number(event.target.value) })
-        }
-      />
-      <br />
-      <label htmlFor="lng">Longitude</label>
-      <input
-        type="number"
-        id="lng"
-        name="lng"
-        value={center.lng}
-        onChange={(event) =>
-          setCenter({ ...center, lng: Number(event.target.value) })
-        }
-      />
-      <h3>{clicks.length === 0 ? "Click on map to add markers" : "Clicks"}</h3>
-      {clicks.map((latLng, i) => (
-        <pre key={i}>{JSON.stringify(latLng.toJSON(), null, 2)}</pre>
-      ))}
-      <button onClick={() => setClicks([])}>Clear</button>
-    </div>
-  );
+  // const form = (
+  //   <div
+  //     style={{
+  //       padding: "1rem",
+  //       flexBasis: "250px",
+  //       height: "100%",
+  //       overflow: "auto"
+  //     }}
+  //   >
+  //     <label htmlFor="zoom">Zoom</label>
+  //     <input
+  //       type="number"
+  //       id="zoom"
+  //       name="zoom"
+  //       value={zoom}
+  //       onChange={(event) => setZoom(Number(event.target.value))}
+  //     />
+  //     <br />
+  //     <label htmlFor="lat">Latitude</label>
+  //     <input
+  //       type="number"
+  //       id="lat"
+  //       name="lat"
+  //       value={center.lat}
+  //       onChange={(event) =>
+  //         setCenter({ ...center, lat: Number(event.target.value) })
+  //       }
+  //     />
+  //     <br />
+  //     <label htmlFor="lng">Longitude</label>
+  //     <input
+  //       type="number"
+  //       id="lng"
+  //       name="lng"
+  //       value={center.lng}
+  //       onChange={(event) =>
+  //         setCenter({ ...center, lng: Number(event.target.value) })
+  //       }
+  //     />
+  //     <h3>{clicks.length === 0 ? "Click on map to add markers" : "Clicks"}</h3>
+  //     {clicks.map((latLng, i) => (
+  //       <pre key={i}>{JSON.stringify(latLng.toJSON(), null, 2)}</pre>
+  //     ))}
+  //     <button onClick={() => setClicks([])}>Clear</button>
+  //   </div>
+  // );
 
   return (
-    <div style={{ display: "flex", height: "100%" }}>
+    // <div className="map" style={{ display: "flex", height: "100%" }}>
+    // {/* <> */}
+    <>
       <Wrapper
         apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
         render={render}
@@ -287,7 +289,9 @@ export const MapComponent: React.VFC = () => {
           ))} */}
         </Map>
       </Wrapper>
-    </div>
+      </>
+    // {/* </> */}
+    // </div>
   );
 };
 interface MapProps extends google.maps.MapOptions {
