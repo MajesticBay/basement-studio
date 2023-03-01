14c3664a39c35c3a183c8b7a6949c87c77eee093
Sergey Sherstobitov <in43sh@gmail.com>
2023-03-01 04:51:53 -0800

    Replace scroll down desktop image with component so we can animate it easier.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
 import scrollMobile from "../images/icons/scroll-mobile.svg";
-import scrollDesktop from "../images/icons/scroll-desktop.svg";
+// import scrollDesktop from "../images/icons/scroll-desktop.svg";
+import { ScrollDownDesktop } from "./ScrollDownDesktop";
 
 export const Hero = () => {
     return (
         <div className="hero">
             <div className="hero__scroll-container">
-                <img className="hero__scroll hero__scroll--mobile jump" src={scrollMobile} alt="scroll mobile" />
-                <img className="hero__scroll hero__scroll--desktop jump" src={scrollDesktop} alt="scroll desktop" />
+                <img className="hero__scroll hero__scroll--mobile animateRoll" src={scrollMobile} alt="scroll mobile" />
+                {/* <img className="hero__scroll hero__scroll--desktop animateRoll" src={scrollDesktop} alt="scroll desktop" /> */}
+                <ScrollDownDesktop className={"hero__scroll hero__scroll--desktop animateRoll"} />
             </div>
         </div>
     )
