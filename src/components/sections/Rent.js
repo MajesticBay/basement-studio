ef1775ab2c29aadcbd749f294ad09e48140f041f
Sergey Sherstobitov <in43sh@gmail.com>
2023-03-03 20:23:33 -0800

    Change card height in for Rent component.

--- src/components/sections/Rent.js
+++ src/components/sections/Rent.js
@@ -40,24 +40,24 @@ export const Rent = () => {
 
     const cards = cardsData.map((card, indexCard) => (
         <CarouselItem>
-        <div className="card" key={indexCard}>
-            <div className="card__img-container">
-                <img className="card__img" src={ card.image } alt={ card.image } />
+            <div className="card rent__card" key={indexCard}>
+                <div className="card__img-container">
+                    <img className="card__img" src={ card.image } alt={ card.image } />
+                </div>
+                <p className="card__title">{ card.title }</p>
+                <p className="card__price">{ card.price }$ / год</p>
+                <ul className="card__list">
+                    {card.list.map((item, index) => (
+                        <li className="card__list-item" key={index}>{item}</li>
+                    ))}
+                </ul>
+                <Btn
+                    className={"card__btn"}
+                    text={"ЗАМОВИТИ"}
+                    arrowDisplayed={false}
+                    dark={false}
+                />
             </div>
-            <p className="card__title">{ card.title }</p>
-            <p className="card__price">{ card.price }$ / год</p>
-            <ul className="card__list">
-                {card.list.map((item, index) => (
-                    <li className="card__list-item" key={index}>{item}</li>
-                ))}
-            </ul>
-            <Btn
-                className={"card__btn"}
-                text={"ЗАМОВИТИ"}
-                arrowDisplayed={false}
-                dark={false}
-            />
-        </div>
         </CarouselItem>
     ))
     return (
@@ -65,14 +65,14 @@ export const Rent = () => {
             <div className="rent__inner" style={bgOverlayStyle}>
                 {/* <div className="rent__content"> */}
                     <h1 className="header">ОРЕНДА</h1>
-                    <div className="rent__card-container--mobile">
+                    <div className="rent__card-container rent__card-container--mobile">
                         <div className="cards">
                             <MobileCarousel>
                                 {cards}
                             </MobileCarousel>
                         </div>
                     </div>
-                    <div className="rent__card-container--desktop">
+                    <div className="rent__card-container rent__card-container--desktop">
                         <div className="cards">
                             {cards}
                         </div>
