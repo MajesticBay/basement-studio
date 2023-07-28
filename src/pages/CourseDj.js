679bd45e273da06165004facd7b746ba17281e26
Sergey Sherstobitov <in43sh@gmail.com>
2023-07-28 03:57:35 -0700

    Start working on course dj page.

--- src/pages/CourseDj.js
+++ src/pages/CourseDj.js
@@ -3,14 +3,18 @@ import { ContactUs } from "../components/sections/ContactUs";
 import { Footer } from "../components/sections/Footer";
 
 export const CourseDj = () => {
-    return (
-        <>
-            <h1 style={{color: "--color-white"}}>DJ COURSE</h1>
-            <GiftCertificate />
-            <ContactUs />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <section className="course-dj-page">
+        <div className="course-dj-page__top-container">
+          <h1 className="header"></h1>
+        </div>
+        <GiftCertificate />
+        <ContactUs />
+        <Footer />
+      </section>
+    </>
+  );
+};
 
 export default CourseDj;
