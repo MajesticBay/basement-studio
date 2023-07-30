7da85ce2cc4369c13b57d887dab64d3e013bdc5f
Sergey Sherstobitov <in43sh@gmail.com>
2023-07-30 05:06:34 -0700

    Add localization for CourseDJ page.

--- src/pages/CourseDj.js
+++ src/pages/CourseDj.js
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { GiftCertificate } from "../components/sections/GiftCertificate";
 import { ContactUs } from "../components/sections/ContactUs";
 import { Footer } from "../components/sections/Footer";
@@ -6,14 +7,15 @@ import courseDjKeyboardSmall from "../images/course-dj-page-colored-small.png";
 import courseDjKeyboardLargeColored from "../images/course-dj-page-colored-larde.png";
 
 export const CourseDj = () => {
+  const { t } = useTranslation();
   return (
     <>
       <section className="course-dj-page">
         <div className="course-dj-page__top-container">
-          <h1 className="header course-dj-page__header">DJ COURSE</h1>
-          <p className="course-dj-page__sign-up">
-            Sign up for a free trial lesson.
-          </p>
+          <h1 className="header course-dj-page__header">
+            {t("courseDjPage.header")}
+          </h1>
+          <p className="course-dj-page__sign-up">{t("courseDjPage.signup")}</p>
           <Btn
             className={"course-dj-page__btn"}
             text={"ЗАРЕЄСТРУВАТИСЬ"}
@@ -21,75 +23,111 @@ export const CourseDj = () => {
             dark={false}
           />
         </div>
-        <img className="course-dj-page__img-mobile" src={courseDjKeyboardSmall} alt="keyboard" />
-        <img className="course-dj-page__img" src={courseDjKeyboardLargeColored} alt="keyboard" />
+        <img
+          className="course-dj-page__img-mobile"
+          src={courseDjKeyboardSmall}
+          alt="keyboard"
+        />
+        <img
+          className="course-dj-page__img"
+          src={courseDjKeyboardLargeColored}
+          alt="keyboard"
+        />
         <div className="course-dj-page__content">
-          <h2 className="course-dj-page__sub-header">Course topics:</h2>
+          <h2 className="course-dj-page__sub-header">
+            {t("courseDjPage.subheader")}
+          </h2>
           <ul className="course-dj-page__list">
-            <li className="course-dj-page__list-item">The basic concept of DJing as an art.</li>
             <li className="course-dj-page__list-item">
-              Functionality and types of professional DJ equipment: mixers,
-              players and effectors (Pioneer Dj, Technics, Allen&Heath).
-            </li>
-            <li className="course-dj-page__list-item">BPM and Music time signature.</li>
-            <li className="course-dj-page__list-item">Sound frequencies. Types and practical use of Equalizers.</li>
-            <li className="course-dj-page__list-item">Beatmatching. Sync by ear.</li>
-            <li className="course-dj-page__list-item">Simple and complex mixing techniques.</li>
-            <li className="course-dj-page__list-item">Process of creating mixes for podcast recording.</li>
-            <li className="course-dj-page__list-item">
-              Building your music library to perform without prepared set.
+              {t("courseDjPage.listItem1")}
             </li>
             <li className="course-dj-page__list-item">
-              Music theory needed for DJing (basics of rhythm, notes, tonality,
-              and the Camelot circle).
-            </li>
-            <li className="course-dj-page__list-item">Audio cables and connectors.</li>
-            <li className="course-dj-page__list-item">
-              Searching skills using labels, streaming services and marketplaces
-              (Bandcamp, SoundCloud, Beatport).
-            </li>
-            <li className="course-dj-page__list-item">Rekordbox: creating playlists and main functionality.</li>
-            <li className="course-dj-page__list-item">
-              Operation of music labels and their interaction with artists.
+              {t("courseDjPage.listItem2")}
             </li>
             <li className="course-dj-page__list-item">
-              Club culture. All you need to know about promoters, booking
-              agents, promo groups, etc.
+              {t("courseDjPage.listItem3")}
             </li>
             <li className="course-dj-page__list-item">
-              Practicing. A lot of practice with the analysis of the smallest
-              details of the art of DJing.
+              {t("courseDjPage.listItem4")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem5")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem6")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem7")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem8")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem9")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem10")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem11")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem12")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem13")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem14")}
+            </li>
+            <li className="course-dj-page__list-item">
+              {t("courseDjPage.listItem15")}
             </li>
           </ul>
           <div className="course-dj-page__cards">
             <div className="course-dj-page__card course-page-card">
-              <p className="course-page-card__header">INDIVIDUAL COURSE</p>
-              <p className="course-page-card__description">*lesson duration 1,5-2 hours</p>
+              <p className="course-page-card__header">
+                {t("courseDjPage.cardHeaderOne")}
+              </p>
+              <p className="course-page-card__description">
+                {t("courseDjPage.cardDescription")}
+              </p>
               <div className="course-page-card__row">
-                <p className="course-page-card__quantity">10 lessons</p>
+                <p className="course-page-card__quantity">
+                  10 {t("courseDjPage.lessons")}
+                </p>
                 <p className="course-page-card__price">300$</p>
               </div>
               <div className="course-page-card__row">
-                <p className="course-page-card__quantity">10 lessons</p>
+                <p className="course-page-card__quantity">
+                  20 {t("courseDjPage.lessons")}
+                </p>
                 <p className="course-page-card__price">300$</p>
               </div>
             </div>
             <div className="course-dj-page__card course-page-card">
-              <p className="course-page-card__header">INDIVIDUAL COURSE</p>
-              <p className="course-page-card__description">*lesson duration 1,5-2 hours</p>
+              <p className="course-page-card__header">
+                {t("courseDjPage.cardHeaderTwo")}
+              </p>
+              <p className="course-page-card__description">
+                {t("courseDjPage.cardDescription")}
+              </p>
               <div className="course-page-card__row">
-                <p className="course-page-card__quantity">10 lessons</p>
-                <p className="course-page-card__price">300$</p>
+                <p className="course-page-card__quantity">
+                  10 {t("courseDjPage.lessons")}
+                </p>
+                <p className="course-page-card__price">450$</p>
               </div>
               <div className="course-page-card__row">
-                <p className="course-page-card__quantity">10 lessons</p>
-                <p className="course-page-card__price">300$</p>
+                <p className="course-page-card__quantity">
+                  20 {t("courseDjPage.lessons")}
+                </p>
+                <p className="course-page-card__price">750$</p>
               </div>
             </div>
           </div>
         </div>
       </section>
-
       <GiftCertificate />
       <ContactUs />
       <Footer />
