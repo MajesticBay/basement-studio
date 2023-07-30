4b3a475c475bc3718c9d36e9f42f3063471ccd0b
Sergey Sherstobitov <in43sh@gmail.com>
2023-07-30 04:23:51 -0700

    Work on DJ course page.

--- src/pages/CourseDj.js
+++ src/pages/CourseDj.js
@@ -2,7 +2,8 @@ import { GiftCertificate } from "../components/sections/GiftCertificate";
 import { ContactUs } from "../components/sections/ContactUs";
 import { Footer } from "../components/sections/Footer";
 import { Btn } from "../components/Btn";
-import courseDjKeyboard from "../images/course-dj-keyboard.png";
+import courseDjKeyboardSmall from "../images/course-dj-page-colored-small.png";
+import courseDjKeyboardLargeColored from "../images/course-dj-page-colored-larde.png";
 
 export const CourseDj = () => {
   return (
@@ -14,63 +15,84 @@ export const CourseDj = () => {
             Sign up for a free trial lesson.
           </p>
           <Btn
-            className={"card__btn course-dj-page__btn"}
+            className={"course-dj-page__btn"}
             text={"ЗАРЕЄСТРУВАТИСЬ"}
             arrowDisplayed={true}
             dark={false}
           />
         </div>
-        <img src={courseDjKeyboard} alt="keyboard" />
-        <div>
-            <h2>Course topics:</h2>
-            <ul>
-                <li>The basic concept of DJing as an art.</li>
-                <li>Functionality and types of professional DJ equipment: mixers, players and effectors (Pioneer Dj, Technics, Allen&Heath).</li>
-                <li>BPM and Music time signature.</li>
-                <li>Sound frequencies. Types and practical use of Equalizers.</li>
-                <li>Beatmatching. Sync by ear.</li>
-                <li>Simple and complex mixing techniques.</li>
-                <li>Process of creating mixes for podcast recording.</li>
-                <li>Building your music library to perform without prepared set.</li>
-                <li>Music theory needed for DJing (basics of rhythm, notes, tonality, and the Camelot circle).</li>
-                <li>Audio cables and connectors.</li>
-                <li>Searching skills using labels, streaming services and marketplaces (Bandcamp, SoundCloud, Beatport).</li>
-                <li>Rekordbox: creating playlists and main functionality.</li>
-                <li>Operation of music labels and their interaction with artists.</li>
-                <li>Club culture. All you need to know about promoters, booking agents, promo groups, etc.</li>
-                <li>Practicing. A lot of practice with the analysis of the smallest details of the art of DJing.</li>
-            </ul>
-            <div className="course-dj-page__cards">
-                <div className="course-dj-page__card">
-                    <p>INDIVIDUAL COURSE</p>
-                    <p>*lesson duration 1,5-2 hours</p>
-                    <div>
-                        <p>10 lessons</p>
-                        <p>300$</p>
-                    </div>
-                    <div>
-                        <p>10 lessons</p>
-                        <p>300$</p>
-                    </div>
-                </div>
-                <div className="course-dj-page__card">
-                    <p>INDIVIDUAL COURSE</p>
-                    <p>*lesson duration 1,5-2 hours</p>
-                    <div>
-                        <p>10 lessons</p>
-                        <p>300$</p>
-                    </div>
-                    <div>
-                        <p>10 lessons</p>
-                        <p>300$</p>
-                    </div>
-                </div>
+        <img className="course-dj-page__img-mobile" src={courseDjKeyboardSmall} alt="keyboard" />
+        <img className="course-dj-page__img" src={courseDjKeyboardLargeColored} alt="keyboard" />
+        <div className="course-dj-page__content">
+          <h2 className="course-dj-page__sub-header">Course topics:</h2>
+          <ul className="course-dj-page__list">
+            <li className="course-dj-page__list-item">The basic concept of DJing as an art.</li>
+            <li className="course-dj-page__list-item">
+              Functionality and types of professional DJ equipment: mixers,
+              players and effectors (Pioneer Dj, Technics, Allen&Heath).
+            </li>
+            <li className="course-dj-page__list-item">BPM and Music time signature.</li>
+            <li className="course-dj-page__list-item">Sound frequencies. Types and practical use of Equalizers.</li>
+            <li className="course-dj-page__list-item">Beatmatching. Sync by ear.</li>
+            <li className="course-dj-page__list-item">Simple and complex mixing techniques.</li>
+            <li className="course-dj-page__list-item">Process of creating mixes for podcast recording.</li>
+            <li className="course-dj-page__list-item">
+              Building your music library to perform without prepared set.
+            </li>
+            <li className="course-dj-page__list-item">
+              Music theory needed for DJing (basics of rhythm, notes, tonality,
+              and the Camelot circle).
+            </li>
+            <li className="course-dj-page__list-item">Audio cables and connectors.</li>
+            <li className="course-dj-page__list-item">
+              Searching skills using labels, streaming services and marketplaces
+              (Bandcamp, SoundCloud, Beatport).
+            </li>
+            <li className="course-dj-page__list-item">Rekordbox: creating playlists and main functionality.</li>
+            <li className="course-dj-page__list-item">
+              Operation of music labels and their interaction with artists.
+            </li>
+            <li className="course-dj-page__list-item">
+              Club culture. All you need to know about promoters, booking
+              agents, promo groups, etc.
+            </li>
+            <li className="course-dj-page__list-item">
+              Practicing. A lot of practice with the analysis of the smallest
+              details of the art of DJing.
+            </li>
+          </ul>
+          <div className="course-dj-page__cards">
+            <div className="course-dj-page__card course-page-card">
+              <p className="course-page-card__header">INDIVIDUAL COURSE</p>
+              <p className="course-page-card__description">*lesson duration 1,5-2 hours</p>
+              <div className="course-page-card__row">
+                <p className="course-page-card__quantity">10 lessons</p>
+                <p className="course-page-card__price">300$</p>
+              </div>
+              <div className="course-page-card__row">
+                <p className="course-page-card__quantity">10 lessons</p>
+                <p className="course-page-card__price">300$</p>
+              </div>
             </div>
+            <div className="course-dj-page__card course-page-card">
+              <p className="course-page-card__header">INDIVIDUAL COURSE</p>
+              <p className="course-page-card__description">*lesson duration 1,5-2 hours</p>
+              <div className="course-page-card__row">
+                <p className="course-page-card__quantity">10 lessons</p>
+                <p className="course-page-card__price">300$</p>
+              </div>
+              <div className="course-page-card__row">
+                <p className="course-page-card__quantity">10 lessons</p>
+                <p className="course-page-card__price">300$</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <GiftCertificate />
-        <ContactUs />
-        <Footer />
       </section>
+
+      <GiftCertificate />
+      <ContactUs />
+      <Footer />
     </>
   );
 };
