acc37b2580594e408d11209dbab2b3021c253033
Joe Siburov <g.siburov@gmail.com>
2023-07-31 23:17:45 +0400

    fix: Recording Studio page fix and refactor

--- src/components/sections/RecordStudio.js
+++ src/components/sections/RecordStudio.js
@@ -5,11 +5,9 @@ import bg from "../../images/bg-record-studio.png";
 
 export const RecordStudio = () => {
     const { t } = useTranslation();
-    const data = {
-        title: `${t('recordStudio.p1')}`,
-        pFirst: `${t('recordStudio.p2')}`,
-        pSecond: `${t('recordStudio.p3')}`
-    }
+
+    const data = ['p1', 'p2', 'p3'].map(p => t(`recordStudio.${p}`));
+    const [title, pFirst, pSecond] = data;
 
     const bgStyle = {
         backgroundImage: `url(${bg})`
@@ -20,58 +18,21 @@ export const RecordStudio = () => {
         backdropFilter: "blur(3px)"
     }
 
-    const cardsData = [
-        {
-            title: `${t('recordStudioCardOne.p1')}`,
-            price: `${t('recordStudioCardOne.price')}`,
-            list: [
-                `${t('recordStudioCardOne.p2')}`,
-                `${t('recordStudioCardOne.p3')}`,
-                `${t('recordStudioCardOne.p4')}`
-            ]
-        },
-        {
-            title: `${t('recordStudioCardTwo.p1')}`,
-            price: `${t('recordStudioCardTwo.price')}`,
-            list: [
-                `${t('recordStudioCardTwo.p2')}`,
-                `${t('recordStudioCardTwo.p3')}`,
-                `${t('recordStudioCardTwo.p4')}`
-            ]
-        },
-        {
-            title: `${t('recordStudioCardThree.p1')}`,
-            price: `${t('recordStudioCardThree.price')}`,
-            list: [
-                `${t('recordStudioCardThree.p2')}`,
-                `${t('recordStudioCardThree.p3')}`,
-            ]
-        }
-    ]
+    const cardKeys = ['p1', 'price', 'p2', 'p3', 'p4'];
+    const cardsData = ['One', 'Two', 'Three'].map(number => {
+        const cardData = cardKeys.map(key => t(`recordStudioCard${number}.${key}`));
+        const [title, price, ...list] = cardData;
+
+        return { title, price, list: list.filter(Boolean) };  // filter out empty translations
+    });
 
     const cards = cardsData.map((card, indexCard) => (
-        // Carousel is here
         <CarouselItem key={indexCard}>
-            <div className="card record-studio__card" >
-                {/* ------------------------------- */}
-                {/* TO DO */}
-                {/* Give each element inside the card a key */}
-                {/* ------------------------------- */}
+            <div className="card record-studio__card">
                 <p className="card__title">{ card.title }</p>
                 <p className="card__price">{ card.price }</p>
                 <ul className="card__list">
-                    {card.list.map((item, index) => (
-                        <li className="card__list-item" key={index}>{item}</li>
-                        // {item === "" ? () : ()}
-                        // {item.length === 0 ? (<li className="card__list-item" key={index}>{item}</li>) : ()}
-                        // {item.length === 0 ? () : ()}
-                        // {item === "" && <li className="card__list-item" key={index}>{item}</li>}
-                        // if (item) {
-                        //     <li className="card__list-item" key={index}>{item}</li>
-                        // } else {
-                        //     <li className="card__list-item" key={index}>{item}</li>
-                        // }
-                    ))}
+                    {card.list.map(item => <li className="card__list-item" key={item}>{item}</li>)}
                 </ul>
                 <Btn
                     className={"card__btn"}
@@ -86,29 +47,15 @@ export const RecordStudio = () => {
     return (
         <div id="record-studio" className="record-studio" style={bgStyle}>
             <div className="record-studio__inner" style={bgOverlayStyle}>
-
                 <div className="record-studio__header-wrapper">
-                    <h1 className="header">
-                        {data.title}
-                    </h1>
-
-                    <p className="record-studio__text">
-                        {data.pFirst}
-                    </p>
-                    <p className="record-studio__text">
-                        {data.pSecond}
-                    </p>
+                    <h1 className="header">{title}</h1>
+                    <p className="record-studio__text">{pFirst}</p>
+                    <p className="record-studio__text">{pSecond}</p>
                 </div>
-                
-                   
-                    <div className="cards mobile">
-                        <Carousel>
-                            {cards}
-                        </Carousel>
-                    </div>
-                <div className="cards desktop">
-                    {cards}
+                <div className="cards mobile">
+                    <Carousel>{cards}</Carousel>
                 </div>
+                <div className="cards desktop">{cards}</div>
             </div>
         </div>
     )
