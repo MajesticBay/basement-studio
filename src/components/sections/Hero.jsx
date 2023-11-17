f1d9ad707c6fb7f821d9e10bf974f99369d7b8cc
Joe Siburov <g.siburov@gmail.com>
2023-11-17 15:51:11 +0400

    logo hidden, iframe loading on width > 1080px

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ScrollDownMobile, ScrollDownDesktop } from '../icons/index.ts'
 
 export const Hero = () => {
+  const [loadVideo, setLoadVideo] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => setLoadVideo(window.innerWidth > 1080)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
-        <div className="hero">
-          <iframe id="video-background" // https://developers.google.com/youtube/player_parameters
-            src="https://www.youtube.com/embed/LYWFL5pGeME?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=LYWFL5pGeME&mute=1"
-            frameBorder="0"
-            allowfullscreen>
-          </iframe>
-            <div className="hero__scroll-container">
-                <ScrollDownMobile />
-                <ScrollDownDesktop />
-            </div>
-        </div>
+    <div className="hero">
+      {loadVideo && (
+        <iframe
+          id="video-background"
+          src="https://www.youtube.com/embed/LYWFL5pGeME?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=LYWFL5pGeME&mute=1"
+          frameBorder="0" // Trick to hide a logo https://jsfiddle.net/s6tp2xfm
+          allowFullScreen
+        />
+      )}
+      <div className="hero__scroll-container">
+        <ScrollDownMobile />
+        <ScrollDownDesktop />
+      </div>
+    </div>
   )
 }
 
