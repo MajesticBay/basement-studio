0415d7f60fc75c68782d242a031a7d852d81e3ac
Joe Siburov <g.siburov@gmail.com>
2023-11-17 15:38:14 +0400

    import video placeholder from Youtube

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,6 +4,11 @@ import { ScrollDownMobile, ScrollDownDesktop } from '../icons/index.ts'
 export const Hero = () => {
   return (
         <div className="hero">
+          <iframe id="video-background" // https://developers.google.com/youtube/player_parameters
+            src="https://www.youtube.com/embed/LYWFL5pGeME?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=LYWFL5pGeME&mute=1"
+            frameBorder="0"
+            allowfullscreen>
+          </iframe>
             <div className="hero__scroll-container">
                 <ScrollDownMobile />
                 <ScrollDownDesktop />
