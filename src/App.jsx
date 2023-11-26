e7ac4451e3a668eb566e122298cb5ab91e26a57c
Joe Siburov <g.siburov@gmail.com>
2023-11-26 15:26:13 +0400

    Privacy page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { PageNotFound } from './pages/PageNotFound'
 import { CourseDj } from './pages/CourseDj'
 import { CourseProduction } from './pages/CourseProduction'
 import { Terms } from './pages/Terms'
+import { Privacy } from './pages/Privacy'
 
 function App () {
   return (
@@ -16,6 +17,7 @@ function App () {
           <Route path="/course-dj" element={<CourseDj />} />
           <Route path="/course-production" element={<CourseProduction />} />
           <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <div className="footer">
-      <a href="/privacy" className="footer__link">Privacy Policy & Cookies Statement</a>
+      <Link className="footer__link" to="/privacy">Privacy Policy & Cookies Statement</Link>
       <Link className="footer__link" to="/terms">Website Terms Of Use</Link>
     </div>
   )
